e4bd2f545c4776bb9fc595677c691a84fca577ad
ankitdoi--coder <ankitdoi82@gmail.com>
2025-04-07 16:03:09 +0530

    new Commit

--- src/main/java/com/example/CRUD/SQL/App/Entity/student.java
+++ src/main/java/com/example/CRUD/SQL/App/Entity/student.java
@@ -36,50 +36,6 @@ public class student
     private float percentage;
 
 
-    // //deafault constructor
-    // public student() {
-    //     super();
-    // }
-
-    // //parameterized constructor
-    // public student(String name, String branch, float percentage) {
-    //     this.name = name;
-    //     this.branch = branch;
-    //     this.percentage = percentage;
-    // }
-    // //Creating Getter and setter
-    // public int getRollno() {
-    //     return rollno;
-    // }
-
-    // public void setRollno(int rollno) {
-    //     this.rollno = rollno;
-    // }
-    // public String getName() {
-    //     return name;
-    // }
-    // public void setName(String name) {
-    //     this.name = name;
-    // }
-    // public String getBranch() {
-    //     return branch;
-    // }
-    // public void setBranch(String branch) {
-    //     this.branch = branch;
-    // }
-    // public float getPercentage() {
-    //     return percentage;
-    // }
-    // public void setPercentage(float percentage) {
-    //     this.percentage = percentage;
-    // }
-
-    // //to string method
-    // @Override
-    // public String toString() {
-    //     return "student [rollno=" + rollno + ", name=" + name + ", branch=" + branch + ", percentage=" + percentage
-    //             + "]";
-    // }
 
 
 }
